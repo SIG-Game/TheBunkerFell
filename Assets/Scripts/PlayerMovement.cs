863af3e16ada37a95f0da919510e68ce1605bda5
adenprince <45126168+adenprince@users.noreply.github.com>
2024-07-31 21:40:31 -0500

    Update player movement and camera perspective
    Also refactor player code.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,68 +1,41 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Different speeds we can modify
-    public float speed = 20f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    [SerializeField] private Tasks tasks;
+    [SerializeField] private float speed;
+    [SerializeField] private int taskDistanceLimit;
 
-    public CharacterController controller;
+    private CharacterController characterController;
+    private int currentTask = -1;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    private Vector3 velocity;
-    private bool isGrounded;
-
-    public Tasks tasks;
-    private int currenTask = -1;
-    public int taskDistanceLimit = 10;
-
-    // Start is called before the first frame update
-    void Start()
-    { }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        characterController = GetComponent<CharacterController>();
+    }
 
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
+    private void Update()
+    {
+        if (currentTask != -1)
         {
-            velocity.y = -2f;
-        }
-
-        //Gets input from keys
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        //Move object
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        //
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-
-        if (currenTask != -1)
-        {
-            if (tasks.GetDistance(GetComponent<Transform>().position, currenTask) >= taskDistanceLimit)
+            if (tasks.GetDistance(GetComponent<Transform>().position, currentTask) >= taskDistanceLimit)
             {
-                tasks.HideTask(currenTask);
-                currenTask = -1;
+                tasks.HideTask(currentTask);
+                currentTask = -1;
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        if (input.sqrMagnitude > 1f)
+        {
+            input.Normalize();
+        }
+
+        characterController.Move(speed * input);
     }
 
     //Detects collision on Player
@@ -71,9 +44,9 @@ public class PlayerMovement : MonoBehaviour
         Task obj = hit.gameObject.GetComponent<Task>();
         if (obj != null && !obj.getDone())
         {
-            currenTask = obj.taskNumber;
+            currentTask = obj.taskNumber;
             //Show specific task for it
-            tasks.ShowTask(currenTask);
+            tasks.ShowTask(currentTask);
         }
     }
 }
