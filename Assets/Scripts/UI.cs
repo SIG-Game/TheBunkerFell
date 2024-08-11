d126781b65c089f33c91ee328573d4e6b3e66708
eky2429 <eky2429@gmail.com>
2024-08-10 20:18:23 -0500

    Many things...
    - Created NEW main scene
    - Implemented UI (wout events for now...)

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,39 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Internal;
+using UnityEngine.Internal;
 using UnityEngine;
 using TMPro;
 
 public class UI : MonoBehaviour
 {
-    int hearts = 0;
-    int helpers = 0;
 
     public TMP_Text heart_text;
     public TMP_Text helper_text;
 
+    private int _hearts;
+    private int _helpers;
+
+    public int Hearts
+    {
+        get
+        {
+            return _hearts;
+        }
+        set
+        {
+            _hearts = value;
+            heart_text.SetText(_hearts.ToString());
+        }
+    }
+
+    public int Helpers
+    {
+        get
+        {
+            return _helpers;
+        }
+        set
+        {
+            _helpers = value;
+            helper_text.SetText(_helpers.ToString());
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        heart_text.SetText(hearts.ToString());
-        helper_text.SetText(helpers.ToString());
+        Hearts = 0;
+        Helpers = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            Hearts++;
+        }
         
-    }
 
-    public void incrementHearts()
-    {
-        hearts++;
-        heart_text.SetText(hearts.ToString());
-    }
-
-    public void incrementHelpers()
-    {
-        helpers++;
-        helper_text.SetText(helpers.ToString());
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Helpers++;
+        }
     }
 }
